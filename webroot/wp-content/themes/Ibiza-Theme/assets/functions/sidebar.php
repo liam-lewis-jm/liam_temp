bd6c7e19f3733321882793decdfff3c37e841cbf
Ryan Neal Blake <Ryan.Blake@jewellerymaker.com>
2016-08-17 15:42:28 +0100

    Fix how to guides

--- webroot/wp-content/themes/Ibiza-Theme/assets/functions/sidebar.php
+++ webroot/wp-content/themes/Ibiza-Theme/assets/functions/sidebar.php
@@ -91,6 +91,17 @@ function joints_register_sidebars() {
 		'after_title' => '</h4>',
 	));
 
+
+	register_sidebar(array(
+		'id' => 'howtoguides',
+		'name' => __('How to Guides Products', 'jointswp'),
+		'description' => __('How to Guides Products.', 'jointswp'),
+		'before_widget' => '<div id="%1$s" class="widget %2$s">',
+		'after_widget' => '</div>',
+		'before_title' => '<h4 class="widgettitle">',
+		'after_title' => '</h4>',
+	));
+
         
         
         
