ae8cc96793bfa923f358c7366f3339e3c1daff4b
Ryan Neal Blake <Ryan.Blake@jewellerymaker.com>
2016-09-19 12:18:05 +0100

    For database

--- webroot/wp-config.php
+++ webroot/wp-config.php
@@ -36,7 +36,7 @@ if( $_SERVER['SERVER_ADDR'] == '192.168.56.101' ){
     /** Database Charset to use in creating database tables. */
     define('DB_CHARSET', 'utf8mb4');
     
-}else if( $_SERVER['SERVER_ADDR'] == '52.51.36.86' ){
+}else if( $_SERVER['SERVER_ADDR'] == '172.31.1.0' ){
 
     /** MySQL database username */
     define('DB_USER', 'ibiza');
