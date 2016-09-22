13d8a2c941d00ebbf1bf5977f09b207b76fdb50c
Ryan Neal Blake <Ryan.Blake@jewellerymaker.com>
2016-09-22 12:31:34 +0100

    Fix proxy

--- webroot/proxy.php
+++ webroot/proxy.php
@@ -49,15 +49,6 @@ if( $quantity <=0 ) {
 
 
 parse_str($cookieStr, $output);
-
-
-if( $quantity <=0 ) {
-     $quantity  = 1;
-}
-
-
-parse_str($cookieStr, $output);
-
  
  $data_string =  '{
     "BasketID"          : 0,
@@ -66,7 +57,7 @@ parse_str($cookieStr, $output);
     "WebsiteId"         : 83,
     "ProductSourceId"   : 83,
     "ProductCode"       : "'. $productCode . '" ,
-    "ProductDetailId"   : '. $productDetailId . ' ,
+    "ProductDetailId"   : "'. $productDetailId . '" ,
     "Quantity"          : '. $quantity .',
     "WishListId"        : null,
     "CurrencyId"        : 1,
